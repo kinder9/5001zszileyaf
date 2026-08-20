c11c520751ca8640919aaa7b61e735ae132203b0
kinder9 <mem098.098.mem@gmail.com>
2026-08-20 17:22:48 +0600

    Part2

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,3 @@
+import React from 'react';
 import Home from './pages/Home/Home';
 export default function App(){ return <Home />; }
--- src/components/YouAre.jsx
+++ src/components/YouAre.jsx
@@ -1,2 +1,3 @@
+import React from 'react';
 const words=['Beautiful.','Kind.','Irreplaceable.','Amazing.','Special.','Loved.'];
 export default function YouAre(){return <section className="you-are section-frame"><p className="kicker">always remember</p><div>{words.map((word,index)=><h2 className="word fade" key={word} style={{'--delay':`${index*70}ms`}}>{word}</h2>)}</div></section>}
